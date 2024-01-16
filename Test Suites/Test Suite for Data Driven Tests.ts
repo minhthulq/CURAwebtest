52b03aac136b55f5b59a98c2c65a0487380b7961
Welcome <Welcome@lqmthu>
2024-01-16 10:48:06 +0700

    update Login test case with Data driven test

---
+++ Test Suites/Test Suite for Data Driven Tests.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite for Data Driven Tests</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>7ed66c61-2a48-479c-ad33-7d4968c96763</testSuiteGuid>
+   <testCaseLink>
+      <guid>ca12513b-22dd-42d5-b2af-288a68b31338</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>6cc482f0-ffff-4608-a8a8-edc4efca6c41</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/TD_Login</testDataId>
+      </testDataLink>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId>6cc482f0-ffff-4608-a8a8-edc4efca6c41</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>username</value>
+         <variableId>6b11f348-4d34-4156-80ae-01aab5e4c737</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>6cc482f0-ffff-4608-a8a8-edc4efca6c41</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>password</value>
+         <variableId>7183ea33-ce4e-4410-a601-2a7d2257f7fe</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
